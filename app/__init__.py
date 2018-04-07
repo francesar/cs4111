c830c21de1acb75da414bba03c9435735f41fe44
cesaribarra10 <cfi2103@columbia.edu>
2018-04-07 00:53:24 -0400

    project code

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,198 @@
-from flask import Flask, render_template
 
-app = Flask(__name__)
+"""
+Columbia's COMS W4111.001 Introduction to Databases
+Example Webserver
+To run locally:
+    python server.py
+Go to http://localhost:8111 in your browser.
+A debugger such as "pdb" may be helpful for debugging.
+Read about it online.
+"""
+import os
+  # accessible as a variable in index.html:
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask_login import LoginManager
+from flask import Flask, request, render_template, g, redirect, Response
 
-@app.route("/")
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+#
+# The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
+#
+# XXX: The URI should be in the format of: 
+#
+#     postgresql://USER:PASSWORD@35.227.79.146/proj1part2
+#
+# For example, if you had username gravano and password foobar, then the following line would be:
+#
+#     DATABASEURI = "postgresql://gravano:foobar@35.227.79.146/proj1part2"
+#
+DATABASEURI = "postgresql://cfi2103:6814@35.227.79.146/proj1part2"
+
+
+#
+# This line creates a database engine that knows how to connect to the URI above.
+#
+engine = create_engine(DATABASEURI)
+
+#
+# Example of running queries in your database
+# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
+#
+engine.execute("""CREATE TABLE IF NOT EXISTS test (
+  id serial,
+  name text
+);""")
+engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+
+
+@app.before_request
+def before_request():
+  """
+  This function is run at the beginning of every web request 
+  (every time you enter an address in the web browser).
+  We use it to setup a database connection that can be used throughout the request.
+
+  The variable g is globally accessible.
+  """
+  try:
+    g.conn = engine.connect()
+  except:
+    print("uh oh, problem connecting to database")
+    import traceback; traceback.print_exc()
+    g.conn = None
+
+@app.teardown_request
+def teardown_request(exception):
+  """
+  At the end of the web request, this makes sure to close the database connection.
+  If you don't, the database could run out of memory!
+  """
+  try:
+    g.conn.close()
+  except Exception as e:
+    pass
+
+
+#
+# @app.route is a decorator around index() that means:
+#   run index() whenever the user tries to access the "/" path using a GET request
+#
+# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
+#
+#       @app.route("/foobar/", methods=["POST", "GET"])
+#
+# PROTIP: (the trailing / in the path is important)
+# 
+# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
+# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
+#
+@app.route('/')
 def index():
-    return render_template("index.html")
+  #
+  # example of a database query
+  #
+  cursor = g.conn.execute("SELECT * FROM users")
+  names = []
+  for result in cursor:
+    print(result)  # can also be accessed using result[0]
+  cursor.close()
+  return render_template("index.html")
+
+@app.route('/home')
+def home():
+  return render_template('home.html')
+
+@app.route('/signup', methods=["POST"])
+def signup():
+  form = request.form
+  
+  name = form['name']
+  username = form['username']
+  password = form['password']
+  email = form['email']
+  address = form['address']
+  zipcode = form['zipcode']
+  phonenumber = form['phonenumber']
+  party = form['party']
+
+  
+  q = text("""INSERT INTO users(uid, name, username, password, email, zipcode, hid)
+    VALUES(:uid, :name, :username, :password, :email, :zipcode, :hid)""")
+  g.conn.execute(q, uid=12234, name=name, 
+    username=username, password=password, 
+    email=email, zipcode=60651, hid=1234456)
+
+  return redirect('/home')
+
+@app.route('/login', methods=["POST"])
+def login():
+  username = request.form['username']
+  password = request.form['password']
+
+  q = text("SELECT * FROM users U WHERE username = :username AND password = :password")
+  cursor = g.conn.execute(q, username=username, password=password)
+
+  print(cursor.returns_rows)
+
+  if cursor.returns_rows:
+    print(cursor)
+    return redirect('/home')
+  else:
+    return redirect('/')
+  
+
+#
+# This is an example of a different path.  You can see it at:
+# 
+#     localhost:8111/another
+#
+# Notice that the function name is another() rather than index()
+# The functions for each app.route need to have different names
+#
+@app.route('/another')
+def another():
+  return render_template("another.html")
+
+
+# Example of adding new data to the database
+@app.route('/add', methods=['POST'])
+def add():
+  name = request.form['name']
+  g.conn.execute('INSERT INTO test(name) VALUES (%s)', name)
+  return redirect('/')
+
 
 
 if __name__ == "__main__":
-    app.run(port=7000)
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using:
+
+        python server.py
+
+    Show the help text using:
+
+        python server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    print("running on %s:%d" % (HOST, PORT))
+    app.run(host=HOST, port=PORT, debug=True, threaded=threaded)
+
+  run()
